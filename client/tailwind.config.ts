a2f91dbdc08edecb80c5a7ef9cf157044ed74409
Nurzamann <nurzaman0001@gmail.com>
2025-07-06 15:37:20 +0700

    mengupdate tailwind terbaru 4.1

--- client/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-export default <Config>{
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: { extend: {} },
-  plugins: [],
-};
